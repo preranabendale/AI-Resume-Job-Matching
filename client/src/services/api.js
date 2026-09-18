d0c0674ec69079328219715fa31b57adc9da9591
Prerana Bendale <preranabendale1@gmail.com>
2026-09-18 17:21:00 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-resume-job-matching-api.onrender.com",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return req;
-});
+);
 
 export default API;
